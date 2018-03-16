d1c6f2a08769791b8b8a7f64ab5fe7ea8466479e
Mario <mse.alvial@gmail.com>
2018-03-16 12:00:26 -0300

    release v1

---
+++ src/main/java/generator/CNPJGenerator.java
@@ -0,0 +1,37 @@
+package generator;
+
+public class CNPJGenerator extends Generator {
+
+    public CNPJGenerator(boolean formatted) {
+        super(formatted, 12);
+    }
+
+    @Override
+    protected void generateLastDigits() {
+        final int[] PESOS_FIRST_DIGIT = new int[]{5, 4, 3, 2, 9, 8, 7, 6, 5, 4, 3, 2};
+        final int[] PESOS_SECOND_DIGIT = new int[]{6, 5, 4, 3, 2, 9, 8, 7, 6, 5, 4, 3, 2};
+        generateDigit(PESOS_FIRST_DIGIT);
+        generateDigit(PESOS_SECOND_DIGIT);
+    }
+
+    @Override
+    protected void format() {
+        StringBuilder stringBuilder = new StringBuilder(this.document);
+        this.document = stringBuilder.insert(2, ".")
+                .insert(6, ".")
+                .insert(10, "/")
+                .insert(15, "-")
+                .toString();
+    }
+
+    private void generateDigit(int[] PESOS) {
+        int result = 0;
+        for (int i = 0; i < this.document.toCharArray().length; i++) {
+            int peso = PESOS[i];
+            char digit = this.document.toCharArray()[i];
+            result += calculateNumber(peso, digit);
+        }
+        setDigit(result);
+    }
+
+}
